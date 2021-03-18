2c68d1141fed5ff0334272c0e960d257cac15ec0
jankub <jankubal@gmail.com>
2021-03-18 19:48:24 +0100

    MVC service added

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -23,15 +23,7 @@ namespace CityInfo.API
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			/*services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
-
-
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);*/
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,19 +33,13 @@ namespace CityInfo.API
 			{
 				app.UseDeveloperExceptionPage();
 			}
-			/*else
-			{
-				app.UseExceptionHandler("/Error");
-			}*/
+            else
+            {
+                app.UseExceptionHandler();
+            }
 
-			/*app.UseStaticFiles();
-			app.UseCookiePolicy();
-
-			app.UseMvc();*/
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello world!");
-			});
+			app.UseMvc();
+            
 		}
 	}
 }
