df17f28a0b702766ee8d19f1d5ac2642375ad1d8
Jan Kubal <JKubal@ra.rockwell.com>
2021-03-24 14:46:32 +0100

    appconfiguration for email services

--- CityInfo/CityInfo.API/Services/CloudMailService.cs
+++ CityInfo/CityInfo.API/Services/CloudMailService.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@com.com";
-        private string _mailFrom = "noreply@com.com";
+        private readonly IConfiguration _configuration;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
             //not real sending
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailServie");
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with CloudMailServie");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message {message}");
         }
--- CityInfo/CityInfo.API/Services/LocalMailService.cs
+++ CityInfo/CityInfo.API/Services/LocalMailService.cs
@@ -1,16 +1,23 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API.Services
 {
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "admin@com.com";
-        private string _mailFrom = "noreply@com.com";
+
+        private readonly IConfiguration _configuration;
+
+        public LocalMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
             //not real sending
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with LocalMailServie");
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with LocalMailServie");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message {message}");
         }
