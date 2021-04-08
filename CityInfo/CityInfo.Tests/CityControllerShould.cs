2b38ec95753bf8295dac6a52b7fd991ba753bbf7
Jan Kubal <JKubal@ra.rockwell.com>
2021-04-08 14:29:59 +0200

    Controller tests - GetCity method

--- CityInfo/CityInfo.Tests/CityControllerShould.cs
+++ CityInfo/CityInfo.Tests/CityControllerShould.cs
@@ -26,8 +26,7 @@ namespace CityInfo.Tests
             var mockRepo = new Mock<ICityInfoRepository>();
             
             mockRepo.Setup(repo => repo.GetCities()).Returns(GetCities());
-            var controlerUT = new CitiesController(mockRepo.Object, mapper);
-
+            var controlerUT = new CitiesController(mockRepo.Object, mapper); 
 
             //Act
             var result = controlerUT.GetCities();
@@ -42,6 +41,86 @@ namespace CityInfo.Tests
         }
 
         
+        [Fact]
+        public void ReturnHttpNoFoundWhenNoCityFound()
+        {
+            //Arrange
+            var configMapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<City, CityWithoutPointsOfInterestDto>();
+            });
+            IMapper mapper = configMapper.CreateMapper();
+
+            var mockRepo = new Mock<ICityInfoRepository>();
+            City city = null;
+            mockRepo.Setup(repo => repo.GetCity(It.IsAny<int>(), It.IsAny<bool>())).Returns(city);
+
+            var controlerUT = new CitiesController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controlerUT.GetCity(4, true);
+            
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+
+        }
+
+        [Fact]
+        public void ReturnOkResultAndOneCityWithPoI()
+        {
+            //Arrange
+            var configMapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CityDto, City>();
+                cfg.CreateMap<City, CityDto>();
+                cfg.CreateMap<PointOfInterest, PointOfInterestDto>();
+                cfg.CreateMap<PointOfInterestDto, PointOfInterest>();
+            });
+            IMapper mapper = configMapper.CreateMapper();
+
+            var mockRepo = new Mock<ICityInfoRepository>();
+            City city = mapper.Map<City>(CitiesDataStore.Current.Cities[0]);
+            mockRepo.Setup(repo => repo.GetCity(It.IsAny<int>(), true)).Returns(city);
+
+            var controlerUT = new CitiesController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controlerUT.GetCity(1, true);
+            OkObjectResult resultOk = (OkObjectResult)result;
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(((CityDto) resultOk.Value).PointsOfInterest);
+
+        }
+
+        [Fact]
+        public void ReturnOkResultAndOneCityWithoutPoI()
+        {
+            //Arrange
+            var configMapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<CityDto, City>();
+                cfg.CreateMap<City, CityDto>();
+                cfg.CreateMap<City, CityWithoutPointsOfInterestDto>();
+                cfg.CreateMap<CityWithoutPointsOfInterestDto, City>();
+                cfg.CreateMap<PointOfInterest, PointOfInterestDto>();
+                cfg.CreateMap<PointOfInterestDto, PointOfInterest>();
+            });
+            IMapper mapper = configMapper.CreateMapper();
+
+            var mockRepo = new Mock<ICityInfoRepository>();
+            City city = mapper.Map<City>(CitiesDataStore.Current.Cities[0]);
+            mockRepo.Setup(repo => repo.GetCity(It.IsAny<int>(), false)).Returns(city);
+
+            var controlerUT = new CitiesController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controlerUT.GetCity(1, false);
+            OkObjectResult resultOk = (OkObjectResult)result;
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            Assert.IsType<CityWithoutPointsOfInterestDto>(resultOk.Value);
+
+        }
+
         private IEnumerable<City> GetCities()
         {
             var citiesWithoutPointsOfInterst = new List<City>();
@@ -58,6 +137,30 @@ namespace CityInfo.Tests
             }
 
             return citiesWithoutPointsOfInterst;
+        }
+
+
+        private City GetCity(int id, bool includePointsOfInterest)
+        {
+            var configMapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<City, CityDto>();
+            });
+            IMapper mapper = configMapper.CreateMapper();
+
+            var cityFromStore = CitiesDataStore.Current.Cities.Find(c => c.Id == id);
+
+            if (cityFromStore == null)
+            {
+                return null;
+            }
+
+            if (includePointsOfInterest)
+            {
+                return mapper.Map<City>(cityFromStore);
+            }
+
+            cityFromStore.PointsOfInterest = null;
+            return mapper.Map<City>(cityFromStore);
         }
 
     }
